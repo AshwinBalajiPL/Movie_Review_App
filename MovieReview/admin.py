432c79795f4ebd875540c56ace0ba0c03791af65
Ashwin17 <pl.ashwinbalaji@gmail.com>
2021-11-21 22:34:23 +0530

    admin complete

--- MovieReview/admin.py
+++ MovieReview/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from MovieReview import models
 # Register your models here.
 class Movie_Review_Admin(admin.ModelAdmin):
-    list_view = ('title','posted_on')
+    list_display = ('title','posted_by','posted_on')
 admin.site.register(models.Movie_Review, Movie_Review_Admin)
